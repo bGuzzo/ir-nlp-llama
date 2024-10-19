3cfc17071fa54b52344199215e221f6aac216683
bGuzzo2 <80220100+bGuzzo@users.noreply.github.com>
2024-10-19 11:11:47 +0200

    Add latex project

---
+++ evluation/evalute_original.sh
@@ -0,0 +1,28 @@
+lm_eval --model hf --model_args pretrained=HUGGINGFACE_MODEL_ID  --tasks hellaswag_it,arc_it  --device cuda:0 --batch_size auto:2
+lm_eval --model hf --model_args pretrained=HUGGINGFACE_MODEL_ID  --tasks m_mmlu_it --num_fewshot 5  --device cuda:0 --batch_size auto:2 
+
+
+# Optimize command
+lm_eval --model hf --model_args pretrained="/home/bruno/Documents/GitHub/ir-nlp-llama/models/llamantino_wiki_all_1,load_in_4bit=True" --tasks hellaswag_it --device cuda:0 --batch_size 1 --limit 0.025 --seed 42 --output_path "/home/bruno/Documents/GitHub/ir-nlp-llama/lm-evaluation-harness/lm_eval_result/"  --log_samples
+
+lm_eval --model hf \
+    --model_args pretrained="swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA,load_in_4bit=True" \
+    --tasks hellaswag_it,arc_it \
+    --device cuda:0 \
+    --batch_size 1 \
+    --limit 0.20 \
+    --seed 42 \
+    --output_path "/home/bruno/Documents/GitHub/ir-nlp-llama/evluation/plain_llamantino3_eval" \
+    --log_samples 
+
+# Not working
+# lm_eval --model hf \
+#     --model_args pretrained="swap-uniba/LLaMAntino-3-ANITA-8B-Inst-DPO-ITA,load_in_4bit=True" \
+#     --tasks m_mmlu_it \
+#     # --num_fewshot 5 \
+#     --device cuda:0 \
+#     --batch_size 1 \
+#     --limit 0.01 \
+#     --seed 42 \
+#     --output_path "/home/bruno/Documents/GitHub/ir-nlp-llama/evluation/plain_llamantino3_eval" \
+#     --log_samples 
